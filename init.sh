f093c544a5a4b241a06ebbf893a141f64fc19feb
areenm <arinmaz@gmail.com>
2024-06-15 19:52:34 +0300

    docker  compose file 4

---
+++ init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Define the replica set name
+REPL_SET="myReplicaSet"
+
+# Wait for the MongoDB servers to be ready
+sleep 10
+
+# Initialize the replica set
+mongosh --host mongo1 --eval "rs.initiate({
+  _id: '$REPL_SET',
+  members: [
+    { _id: 0, host: 'mongo1:27017' },
+    { _id: 1, host: 'mongo2:27017' },
+    { _id: 2, host: 'mongo3:27017' }
+  ]
+})"
