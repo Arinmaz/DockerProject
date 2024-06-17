a1af4acb83995e501ded9d307d8f586bca64ef2c
areenm <arinmaz@gmail.com>
2024-06-17 08:33:56 +0300

    touches

--- mongo/init.sh
+++ mongo/init.sh
@@ -1,7 +1,16 @@
-#!/bin/bash
 
 # Define the replica set name
-REPL_SET="myReplicaSet"
+REPL_SET="mongo-replica-set"
+
+# Wait for the MongoDB servers to be ready
+sleep 10
 
 # Initialize the replica set
-mongo --host mongo1 --eval "rs.initiate({_id: '$REPL_SET', members: [{_id: 0, host: 'mongo1:27017'}, {_id: 1, host: 'mongo2:27017'}, {_id: 2, host: 'mongo3:27017'}]})"
+mongosh --host mongo1 --eval "rs.initiate({
+  _id: '$REPL_SET',
+  members: [
+    { _id: 0, host: 'mongo1:27017' },
+    { _id: 1, host: 'mongo2:27017' },
+    { _id: 2, host: 'mongo3:27017' }
+  ]
+})"
